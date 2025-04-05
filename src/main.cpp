bda1ad0f205a3fd0ff39b2bae5eb7be49b867579
Samuel Knox <47692324+SamJCKnox@users.noreply.github.com>
2025-04-05 20:51:49 +0100

    Needs making a bit more sense

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,11 @@ AllSensors_AUAV pressureSensor(&Wire, AllSensors_AUAV::SensorPressureRange::L10D
 
 DroneCAN dronecan;
 
-unsigned long looptime = 0;
+unsigned long looptime1 = 0;
+unsigned long looptime2 = 0;
 unsigned long now = 0;
-unsigned long last_msg_time = 0;
+unsigned long last_msg_time_diff = 0;
+unsigned long last_msg_time_abs = 0;
 
 /*
 This function is called when we receive a CAN message, and it's accepted by the shouldAcceptTransfer function.
@@ -37,12 +39,12 @@ static bool shouldAcceptTransfer(const CanardInstance *ins,
     return false || DroneCANshoudlAcceptTransfer(ins, out_data_type_signature, data_type_id, transfer_type, source_node_id);
 }
 
-void readSensor(AllSensors_AUAV::SensorType type)
+uint8_t readSensor(AllSensors_AUAV::SensorType type)
 {
 
     if (!pressureSensor.isBusy(type))
     {
-        pressureSensor.readData(type);
+        uint8_t status = pressureSensor.readData(type);
 
         // Restart measurement
         pressureSensor.startMeasurement(type, AllSensors_AUAV::MeasurementType::AVERAGE8);
@@ -50,36 +52,32 @@ void readSensor(AllSensors_AUAV::SensorType type)
         switch (type)
         {
         case AllSensors_AUAV::SensorType::DIFFERENTIAL:
+            last_msg_time_diff = pressureSensor.last_message_diff;
             pressureSensor.last_message_diff = now;
         break;
         case AllSensors_AUAV::SensorType::ABSOLUTE:
+            last_msg_time_abs = pressureSensor.last_message_abs;
             pressureSensor.last_message_abs = now;
         break;
         default:
             break;
         }
-        return;
+        return status;
     }
 
-    switch (type)
+    // If we haven't received a message in the last 100ms, restart the measurement
+    if (now - last_msg_time_diff > 150)
     {
-    case AllSensors_AUAV::SensorType::DIFFERENTIAL:
-        last_msg_time = pressureSensor.last_message_diff;
-        pressureSensor.last_message_diff = now;
-        break;
-    case AllSensors_AUAV::SensorType::ABSOLUTE:
-        last_msg_time = pressureSensor.last_message_abs;
-        pressureSensor.last_message_abs = now;
-        break;
-    default:
-        break;
+        pressureSensor.startMeasurement(AllSensors_AUAV::SensorType::DIFFERENTIAL, AllSensors_AUAV::MeasurementType::AVERAGE8);
+        dronecan.debug("No Diff Reading", 0);
     }
-
-    // If we haven't received a message in the last second, restart the measurement
-    if (now - last_msg_time > 1000)
+    // If we haven't received a message in the last 100ms, restart the measurement
+    if (now - last_msg_time_abs > 150)
     {
-        pressureSensor.startMeasurement(type, AllSensors_AUAV::MeasurementType::AVERAGE8);
+        pressureSensor.startMeasurement(AllSensors_AUAV::SensorType::ABSOLUTE, AllSensors_AUAV::MeasurementType::AVERAGE4);
+        dronecan.debug("No Abs Reading", 0);
     }
+    return 0;
 
 }
 
@@ -87,6 +85,7 @@ void setup()
 {
     Serial.begin(115200);
     Serial.println("Node Start");
+    delay(100);
 
     Wire.setSCL(PIN_WIRE_SCL);
     Wire.setSDA(PIN_WIRE_SDA);
@@ -97,10 +96,23 @@ void setup()
     pressureSensor.setPressureUnit(AllSensors_AUAV::PressureUnit::PASCAL);
     pressureSensor.setTemperatureUnit(AllSensors_AUAV::TemperatureUnit::KELVIN);
 
+    pressureSensor.readStatus(AllSensors_AUAV::SensorType::DIFFERENTIAL);
+    delay(100);
+    pressureSensor.readStatus(AllSensors_AUAV::SensorType::ABSOLUTE);
+
+    delay(100);
+
+    pressureSensor.readData(AllSensors_AUAV::SensorType::DIFFERENTIAL);
+    delay(100);
+    pressureSensor.readData(AllSensors_AUAV::SensorType::ABSOLUTE);
+
+    delay(100);
 
     pressureSensor.startMeasurement(AllSensors_AUAV::SensorType::DIFFERENTIAL, AllSensors_AUAV::MeasurementType::AVERAGE8);
-    pressureSensor.startMeasurement(AllSensors_AUAV::SensorType::ABSOLUTE, AllSensors_AUAV::MeasurementType::AVERAGE8);
+    delay(100);
+    pressureSensor.startMeasurement(AllSensors_AUAV::SensorType::ABSOLUTE, AllSensors_AUAV::MeasurementType::AVERAGE4);
     
+    delay(100);
 
     dronecan.init(onTransferReceived, shouldAcceptTransfer);
 
@@ -110,22 +122,27 @@ void setup()
 void loop()
 {
     now = millis();
-
+    char error_msg[50];
 
     // send our battery message at 10Hz
-    if (now - looptime > 50)
+    if (now - looptime2 > 100)
     {
-        looptime = millis();
-        readSensor(AllSensors_AUAV::SensorType::ABSOLUTE);
         readSensor(AllSensors_AUAV::SensorType::DIFFERENTIAL);
-        
+    }
 
+    if (now - looptime1 > 100)
+    {
+        looptime1 = millis();
+        looptime2 = millis() + 50;
+
+        readSensor(AllSensors_AUAV::SensorType::ABSOLUTE);
+        
         // send air data message
         uavcan_equipment_air_data_RawAirData air_data{};
         air_data.differential_pressure = pressureSensor.pressure_d; // in Pascals
         air_data.differential_pressure_sensor_temperature = pressureSensor.temperature_d; // Kelvin
         air_data.static_pressure = pressureSensor.pressure_a; // in Pascals
-        air_data.static_air_temperature = pressureSensor.temperature_a; // Kelvin
+        air_data.static_pressure_sensor_temperature = pressureSensor.temperature_a; // Kelvin
 
         uint8_t buffer[UAVCAN_EQUIPMENT_AIR_DATA_RAWAIRDATA_MAX_SIZE];  // this is the maximum size of the message
         uint32_t len = uavcan_equipment_air_data_RawAirData_encode(&air_data, buffer);
